d4ca5727c7c276a465df793422c79e11b9c36d79
CagyJ <josh.jiao.322@gmail.com>
2022-12-26 00:07:47 +0100

    ecdsa - private key is used by client

--- ecdsa-node/server/index.js
+++ ecdsa-node/server/index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "044ec73105d4829013e531dc32adcdaf42dd2bfbb7a6a0c8f2d9695b5cb871d23b059a1b37ed9566540e21ac462175f0111319a30c439a742016f44d23b3cf4c73": 100,
+  "0411d91e5cdff0232883182e10733ce7923d8d37ebe803757bd851bfa097a902798bb5c09263828e9d9e71fd194ae1afd6f39a6d9eb7591781ab106f0e40af7916": 50,
+  "0496c391f1bd1da253dffa2c942d2b3e0bdb5707f328ed0f7aa09e3635346d8449a4258865f9e94f00f2a4f66e5257363229150518f8bdd571496c155a2a836f89": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,6 +19,7 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
+  // TODO get a signature from client side, and recover the public key from it
   const { sender, recipient, amount } = req.body;
 
   setInitialBalance(sender);
